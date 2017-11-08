cf9024ab64672a3bc036aff6c6c530be93fec345
Ricardo Matos <matosonline@gmail.com>
2017-11-08 00:06:30 +0000

    define script in head

---
+++ index.php
@@ -0,0 +1,20 @@
+
+<!--HEAD-->
+
+    <?php
+    
+        define('__ROOT__', dirname(__FILE__));
+        require_once (__ROOT__."templates/head.php");
+    
+    ?>
+
+<!--/HEAD-->
+<!--NAV-->
+
+<!--/NAV-->
+<!--CONTENT-->
+
+<!--/CONTENT-->
+<!--FOOTER-->
+
+<!--/FOOTER-->    
